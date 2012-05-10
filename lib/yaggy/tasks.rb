b4abfbdbfd83c5c8511ee2d26b1997da028e2ea9
osheroff <ben@zendesk.com>
2012-05-10 16:33:56 -0700

    document the rake tasks

--- lib/yaggy/tasks.rb
+++ lib/yaggy/tasks.rb
@@ -1,14 +1,20 @@
 require 'rake'
 
 namespace :release do
+  desc "release a patch revision"
   task :patch do
     Yaggy::Release.release(:patch)
   end
+
+  desc "release a minor revision"
   task :minor do
     Yaggy::Release.release(:minor)
   end
+
+  desc "release a major revision"
   task :major do
     Yaggy::Release.release(:major)
   end
 end
+desc "release a patch revision"
 task :release => "release:patch"
